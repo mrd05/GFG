42080e07455a97d26360f94f148e02d7335915f7
mrd05 <45844843+mrd05@users.noreply.github.com>
2019-02-09 23:15:22 +0530

    Add files via upload

---
+++ ChocolateDistributionProblem.java
@@ -0,0 +1,42 @@
+package arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class ChocolateDistributionProblem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		try {
+			int t = Integer.parseInt(br.readLine());
+			for (int i = 0; i < t; i++) {
+				int n = Integer.parseInt(br.readLine());
+				int arr[] = new int[n];
+				String str[] = br.readLine().split(" ");
+				int m = Integer.parseInt(br.readLine());
+				for (int j = 0; j < n; j++)
+					arr[j] = Integer.parseInt(str[j]);
+				Arrays.sort(arr);
+
+				int min = Integer.MAX_VALUE;
+				for (int j = 0; j <= n - m; j++) {
+					if ((arr[j + m - 1] - arr[j]) < min) {
+						min = arr[j + m - 1] - arr[j];
+					}
+				}
+				System.out.println(min);
+			}
+
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
